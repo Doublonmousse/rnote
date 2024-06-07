e4c2bd8a8ca87caddd37421e8b50edb84c72129c
Doublonmousse <115779707+Doublonmousse@users.noreply.github.com>
2024-06-07 18:38:31 +0200

    use exclusively `opener` and add an action to open the workspace folder

--- crates/rnote-ui/src/dialogs/export.rs
+++ crates/rnote-ui/src/dialogs/export.rs
@@ -256,11 +256,11 @@ pub(crate) async fn dialog_export_doc_w_prefs(appwindow: &RnAppWindow, canvas: &
                                     appwindow.overlays().dispatch_toast_error(&gettext("Exporting document failed"));
                                     return;
                             };
-
-                            if let Err(e) = open::that(&folder_path_string) {
-                                tracing::error!("Opening the parent folder '{folder_path_string}' in the file manager failed, Err: {e:?}");
-                                appwindow.overlays().dispatch_toast_error(&gettext("Failed to open the file in the file manager"));
-                            }
+                                if let Err(e) = opener::open(&folder_path_string) {
+                                    tracing::error!("Opening the parent folder '{folder_path_string}' in the file manager failed, Err: {e:?}");
+                                    appwindow.overlays().dispatch_toast_error(&gettext("Failed to open the file in the file manager"));
+                                }
+                            })
                         }
                     ), crate::overlays::TEXT_TOAST_TIMEOUT_DEFAULT, &mut None);
                     appwindow.overlays().progressbar_finish();
@@ -587,7 +587,7 @@ pub(crate) async fn dialog_export_doc_pages_w_prefs(appwindow: &RnAppWindow, can
                                 return;
                             };
 
-                            if let Err(e) = open::that(&folder_path_string) {
+                            if let Err(e) = opener:open(&folder_path_string) {
                                 tracing::error!("Opening the parent folder '{folder_path_string}' in the file manager failed, Err: {e:?}");
                                 appwindow.overlays().dispatch_toast_error(&gettext("Failed to open the file in the file manager"));
                             }
@@ -883,7 +883,7 @@ pub(crate) async fn dialog_export_selection_w_prefs(appwindow: &RnAppWindow, can
                                         return;
                                 };
 
-                                if let Err(e) = open::that(&folder_path_string) {
+                                if let Err(e) = opener::open(&folder_path_string) {
                                     tracing::error!("Opening the parent folder '{folder_path_string}' in the file manager failed, Err: {e:?}");
                                     appwindow.overlays().dispatch_toast_error(&gettext("Failed to open the file in the file manager"));
                                 }
